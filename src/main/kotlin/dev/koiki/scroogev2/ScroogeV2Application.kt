c84ba768c89b77fb2d8c4406c940d2efa7b10507
takaaki.iida <takiida0605@gmail.com>
2019-07-22 17:47:06 +0900

    first commit

---
+++ src/main/kotlin/dev/koiki/scroogev2/ScroogeV2Application.kt
@@ -0,0 +1,11 @@
+package dev.koiki.scroogev2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScroogeV2Application
+
+fun main(args: Array<String>) {
+	runApplication<ScroogeV2Application>(*args)
+}
---
+++ src/test/kotlin/dev/koiki/scroogev2/ScroogeV2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.koiki.scroogev2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScroogeV2ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
