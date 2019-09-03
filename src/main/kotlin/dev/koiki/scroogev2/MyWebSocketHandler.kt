34415dc48142949e9c2b62ce268cca701c21432c
takaaki.iida <takiida0605@gmail.com>
2019-09-03 18:41:44 +0900

    message

--- src/main/kotlin/dev/koiki/scroogev2/MyWebSocketHandler.kt
+++ src/main/kotlin/dev/koiki/scroogev2/MyWebSocketHandler.kt
@@ -4,32 +4,62 @@ import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.HandlerMapping
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping
 import org.springframework.web.reactive.socket.WebSocketHandler
 import org.springframework.web.reactive.socket.WebSocketSession
-import reactor.core.publisher.Mono
-import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping
-import java.util.HashMap
-import org.springframework.web.reactive.HandlerMapping
-import org.springframework.web.reactive.socket.server.upgrade.ReactorNettyRequestUpgradeStrategy
-import org.springframework.web.reactive.socket.server.support.HandshakeWebSocketService
 import org.springframework.web.reactive.socket.server.WebSocketService
+import org.springframework.web.reactive.socket.server.support.HandshakeWebSocketService
 import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter
+import org.springframework.web.reactive.socket.server.upgrade.ReactorNettyRequestUpgradeStrategy
+import reactor.core.publisher.Mono
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.CopyOnWriteArraySet
 
 @Component
 class MyWebSocketHandler : WebSocketHandler {
     private val log = LoggerFactory.getLogger(this::class.java)
+    private val sessionPoolMap = ConcurrentHashMap<String, CopyOnWriteArraySet<WebSocketSession>>()
 
     override fun handle(session: WebSocketSession): Mono<Void> {
-        log.info("session established, sessionId: ${session.id}")
+        val eventId = session.handshakeInfo.uri.query
+        log.debug("session established, sessionId: ${session.id}, eventId: $eventId")
+        addSessionToPool(eventId, session)
 
         return session.receive()
             .doOnNext {
-                log.info("message comes, sessionId: ${session.id}, message: ${it.payloadAsText}")
+                val payload = it.payloadAsText
+                log.debug("message comes, sessionId: ${session.id}, message: $payload")
+                session.send(Mono.just(session.textMessage("hey! I am here!, $payload"))).subscribe()
             }
             .doFinally {
-                log.info("terminated, sessionId: ${session.id}")
+                log.debug("terminated, sessionId: ${session.id}")
+                removeSessionFromPool(eventId, session)
             }
             .then()
+    }
+
+    private fun addSessionToPool(eventId: String, session: WebSocketSession) {
+        if (sessionPoolMap[eventId] == null)
+            sessionPoolMap[eventId] = CopyOnWriteArraySet()
+
+        sessionPoolMap[eventId]!!.add(session)
+
+        if (log.isDebugEnabled)
+            log.debug("sessionPoolMap: $sessionPoolMap")
+    }
+
+    private fun removeSessionFromPool(eventId: String, session: WebSocketSession) {
+        val sessions = sessionPoolMap[eventId]
+        if (sessions != null) {
+            sessions.remove(session)
+            if (sessions.isEmpty())
+                sessionPoolMap.remove(eventId)
+        }
+
+        if (log.isDebugEnabled)
+            log.debug("sessionPoolMap: $sessionPoolMap")
     }
 }
 
